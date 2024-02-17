4af8265f17d5c37db7b221dd9e9cc4b321065d63
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-18 00:41:50 +0530

    set the corousel and image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,49 @@
 import React from 'react'
 
-export default function Home() {
+// adding style
+import '../Styles/Home.css';
+
+// adding images
+import Slide01 from '../Assets/Illustration-a5f0019d.png';
+
+function Home() {
     return (
-        <div>Home</div>
+        <div>
+          <div id="carouselExampleIndicators" class="carousel slide bg-light home">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div className=''>
+                  <h1 className=''>Lessons and insights<span>from 8 years</span></h1>
+                  <p>this is a corousel component in bootstrap, it's easy to use</p>
+                  <button type='button'>Register</button>
+                </div>
+                <div className=''>
+                  <img src={Slide01} alt='' />
+                </div>
+              </div>
+              <div class="carousel-item">
+                
+              </div>
+              <div class="carousel-item">
+                
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
     )
 }
+
+export default Home;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ import '../Styles/Navbar.css';
 // react icons
 import { FaXmark , FaBars } from "react-icons/fa6";
 
-function Navbar() {
+const Navbar = ({children}) => {
     const[isOpen,setIsOpen] = useState(false);
     const[isMenuOpen,setIsMenuOpen] = useState(false);
 
@@ -54,17 +54,51 @@ function Navbar() {
     ]
 
     return (
-        <header className='w-100'>
-            <nav className='navbar_ w-100 p-2'>
-                <div className='d-flex justify-content-between align-items-center'>
-                    {/* navbar log */}
-                    <a href='#' className='navbar-logo'><span className='navbar-logo-text'>Expensy</span></a>
+        <div>
+            <header className='w-100'>
+                <nav className='navbar_ w-100 p-2'>
+                    <div className='d-flex justify-content-between align-items-center'>
+                        {/* navbar log */}
+                        <a href='#' className='navbar-logo'><span className='navbar-logo-text'>Expensy</span></a>
 
-                    {/* navbar items for large screen */}
-                    <ul 
-                        style={{display: isOpen ? "none" : "flex",
-                                flexDirection: "row"}} 
-                        className='navbar-items gap-4'>
+                        {/* navbar items for large screen */}
+                        <ul 
+                            style={{display: isOpen ? "none" : "flex",
+                                    flexDirection: "row"}} 
+                            className='navbar-items gap-4'>
+                            {
+                                navItems.map((item,index) => (
+                                    <NavLink to={item.path} key={index}>
+                                        {item.name}
+                                    </NavLink>
+                                ))
+                            }
+                        </ul>
+
+                        {/* navbar login-signup for large screen */}
+                        <div style={{display: isOpen ? "none" : "flex"}} className='navbar-login-singup gap-4'>
+                            <a href='#' className='navbar-login'>Login</a>
+                            <button type='button' className='navbar-sign-up'>Sign Up</button>
+                        </div>
+
+                        {/* menu button for mobile divice */}
+                        <div style={{display: isOpen ? "block" : "none"}} className='menubar'>
+                            <button 
+                                onClick={toggleMenu}
+                                className='menubar-icon'>
+                                {
+                                    isMenuOpen ? (<FaXmark/>) : (<FaBars/>)
+                                }
+                            </button>
+                        </div>
+
+                    
+                    </div>
+
+                    {/* navbar items for mobile device */}
+                    <div
+                        style={{display: isMenuOpen && isOpen ? "flex" : "none", flexDirection: "column", alignItems: "center"}}
+                        className='navbar-menu-body'>
                         {
                             navItems.map((item,index) => (
                                 <NavLink to={item.path} key={index}>
@@ -72,43 +106,14 @@ function Navbar() {
                                 </NavLink>
                             ))
                         }
-                    </ul>
-
-                    {/* navbar login-signup for large screen */}
-                    <div style={{display: isOpen ? "none" : "flex"}} className='navbar-login-singup gap-4'>
-                        <a href='#' className='navbar-login'>Login</a>
-                        <button type='button' className='navbar-sign-up'>Sign Up</button>
                     </div>
 
-                    {/* menu button for mobile divice */}
-                    <div style={{display: isOpen ? "block" : "none"}} className='menubar'>
-                        <button 
-                            onClick={toggleMenu}
-                            className='menubar-icon'>
-                            {
-                                isMenuOpen ? (<FaXmark/>) : (<FaBars/>)
-                            }
-                        </button>
-                    </div>
+                </nav>
+            </header>
 
-                    
-                </div>
+            <main>{children}</main>
 
-                {/* navbar items for mobile device */}
-                <div
-                    style={{display: isMenuOpen && isOpen ? "flex" : "none", flexDirection: "column", alignItems: "center"}}
-                    className='navbar-menu-body'>
-                    {
-                        navItems.map((item,index) => (
-                            <NavLink to={item.path} key={index}>
-                                {item.name}
-                            </NavLink>
-                        ))
-                    }
-                </div>
-
-            </nav>
-        </header>
+        </div>
     )
 }
 
