a044cebc3d7cf8b1c60705f98b73f1b8043e420e
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-24 23:58:52 +0530

    Now, Applying Bootstrap Style for Structure...

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,22 @@
 import React from 'react'
 
+// adding style
+import '../Styles/Contact.css';
+
 function Contact() {
     return (
-        <section className=''>
-            <div className=''>
-                <div className=''>
-                    <h1 className=''>Contact</h1>
-                    <div className=''>
-                        <input type='text' placeholder='First Name' className=''></input>
+        <section className='w-100 vh-100 d-flex flex-column justify-content-center'>
+            <div className='row contact-container mx-5 my-5'>
+                <div className='col card d-flex flex-column align-items-center justify-content-center mx-5 py-3'>
+                    <h1 className='contact-title my-3'>Contact</h1>
+                    <div className='contact-input my-3'>
+                        <input type='text' placeholder='First Name' className='contact-input-field px-3 py-1'></input>                                                                         
                     </div>
-                    <div className=''>
-                        <input type='text' placeholder='Email Address' className=''></input>
+                    <div className='contact-input my-3'>
+                        <input type='text' placeholder='Email Address' className='contact-input-field px-3 py-1'></input>
                     </div>
-                    <div className=''>
-                        <button type='submit'>Submit</button>                                                                                     
+                    <div className='contact-submit my-3'>
+                        <button type='submit' className='px-2 py-1'>Submit</button>                                                                                     
                     </div>
                 </div>
             </div>
